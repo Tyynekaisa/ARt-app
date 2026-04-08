51d7d20450f619b5dcb88756f9d6dd7d7d09604e
Tyynekaisa <annakaisa.juhola@gmail.com>
2026-04-08 20:23:55 +0300

    art selection menu update

--- js/menuBuilder.js
+++ js/menuBuilder.js
@@ -1,34 +1,42 @@
 // menuBuilder.js
-import { loadModelData } from 'https://tyynekaisa.github.io/ARt-app/js/dataLoader.js';
-import { selectModel } from 'https://tyynekaisa.github.io/ARt-app/js/artwall.js';
+import { loadModelData } from 'https://tyynekaisa.github.io/ARt-app/js/dataLoader.js'
+import { selectModel } from 'https://tyynekaisa.github.io/ARt-app/js/artwall.js'
 
 export function buildModelMenu() {
-  const list = document.getElementById('modelList');
+  const list = document.getElementById('modelList')
   
   loadModelData().then(data => {
     console.log(data)
 
     for (const key in data) {
-        const item = data[key]
+      const item = data[key]
 
-        const li = document.createElement('li');
+      const li = document.createElement('li')
 
-        const modelPath = `https://tyynekaisa.github.io/ARt-app/assets/models/paintings/${item.model}`;
-        const imagePath = `https://tyynekaisa.github.io/ARt-app/assets/images/targetImages/${item.image}`;
+      const modelPath = `https://tyynekaisa.github.io/ARt-app/assets/models/paintings/${item.model}`
+      const imagePath = `https://tyynekaisa.github.io/ARt-app/assets/images/targetImages/${item.image}`
 
-        li.dataset.model = item.model;
+      li.dataset.model = item.model
 
-        const img = document.createElement('img');
-        img.src = imagePath;
-        img.classList.add('menu-image');
+      const img = document.createElement('img')
+      img.src = imagePath;
+      img.classList.add('menu-image')
+      img.classList.add('selected')
 
-        li.appendChild(img);
-        list.appendChild(li);
+      li.appendChild(img)
+      list.appendChild(li)
 
-        li.addEventListener("click", () => {
-            selectModel(modelPath);
-            console.log("Valittu teos:", modelPath);
-        })
+      li.addEventListener("click", () => {
+          // 1. Poistetaan 'selected' kaikilta muilta li-elementeiltä
+          const allItems = list.querySelectorAll('img')
+          allItems.forEach(el => el.classList.remove('selected'))
+
+          // 2. Lisätään 'selected' klikatulle elementille
+          img.classList.add('selected')
+
+          selectModel(modelPath)
+          console.log("Valittu teos:", modelPath)
+      })
     }
   })
 }
